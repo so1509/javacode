689041c33e39cf68a5e980d30a989bb2e1ede1e8
so1509 <32246608+so1509@users.noreply.github.com>
2018-09-19 16:57:40 +0200

    Update App.java

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://140.86.15.104:3000/shield/33/45/red/expose";
+        String url = "http://140.86.15.104:3000/shield/33/45/red/so1509";
         CloseableHttpResponse response = null;
  
         try {
